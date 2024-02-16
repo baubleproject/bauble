52c0a7011eec6884731481c6aa84867d45b99ad2
Toyehamzat <anibiseun@gmail.com>
2024-02-16 15:52:39 +0100

    made no changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,28 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/providers/Provider";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { ModeToggle } from "@/components/ui/theme-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Bauble",
-    description: "accelerate university projects",
+  title: "Bauble",
+  description: "accelerate university projects",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <ClerkProvider>
-            <html lang="en" suppressHydrationWarning >
-                <body className={`${inter.className} transition-colors duration-300`}>
-                    <Provider>
-                        {children}
-                    </Provider>
-                </body>
-            </html>
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} transition-colors duration-300`}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
