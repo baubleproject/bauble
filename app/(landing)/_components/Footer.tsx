f47107f1ec6b86e79f37383e61f72782b2ee6bd8
Toyehamzat <anibiseun@gmail.com>
2024-02-17 10:15:30 +0100

    edited footer componet

--- app/(landing)/_components/Footer.tsx
+++ app/(landing)/_components/Footer.tsx
@@ -4,19 +4,19 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 export const Footer = () => {
-    return (
-        <div className="fixed bottom-0 w-full p-4 dark:border-t border-neutral-900  dark:bg-black dark:text-white font-semibold">
-            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-                <Logo />
-                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-                    <Button size="sm" variant="ghost">
-                        Privacy Policy
-                    </Button>
-                    <Button size="sm" variant="ghost">
-                        Terms of Service
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div className="mt-auto w-full p-4 dark:border-t border-neutral-900  dark:bg-black dark:text-white font-semibold">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Service
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
