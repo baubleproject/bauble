0826b662b55b09f38e005a3fb959a9cf26454b06
Toyehamzat <anibiseun@gmail.com>
2024-02-17 10:15:05 +0100

    sign in metadata

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Sign In",
+};
 export default function Page() {
-    return (
-        <section className="h-full flex items-center justify-center">
-            <SignIn />
-        </section>
-    )
+  return (
+    <section className="h-full flex items-center justify-center">
+      <SignIn />
+    </section>
+  );
 }
