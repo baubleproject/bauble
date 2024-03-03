84b60afb5ffec2d8039db5acb3088c90a703b115
sijirama <gbemilesanmi@gmail.com>
2024-03-03 01:24:28 +0100

    member map stlying

--- app/(main)/project/[id]/_components/OverviewPage.tsx
+++ app/(main)/project/[id]/_components/OverviewPage.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { cn } from '@/lib/utils'
 import { MemberandProfile } from '@/type/MemberandProfile'
-import {  Task } from '@prisma/client'
+import { Task } from '@prisma/client'
 import React, { HTMLAttributes } from 'react'
 
 interface OverviewBoardProps extends HTMLAttributes<HTMLDivElement> {
@@ -12,20 +12,21 @@ interface OverviewBoardProps extends HTMLAttributes<HTMLDivElement> {
 export const OverviewPage = ({ members, tasks, className, ...props }: OverviewBoardProps) => {
     return (
         <section {...props} className={cn("", className)}>
-            Overview page
+            {/*Overview page*/}
             {/*
                 tasks?.map((t) => (
                     <p key={t.id}>{t.name}:{t.priority}</p>
                 ))
             */}
             <p className='text-lg font-semibold -tracking-wider'>Project Roles</p>
-            <div className='w-full min-h-48 bg-red-600 flex items-center justify-start'>
+            <div className='w-full min-h-14 flex items-start justify-start py-2 gap-3'>
                 {
                     members?.map((t) => (
-                        <div key={t.id} className='px-3 py-1 flex items-center bg-zinc-300 rounded-xl'>
+                        <div key={t.id} className='px-2 py-3 flex gap-2 items-center bg-zinc-300 dark:bg-zinc-800 rounded-lg'>
                             <div className="cursor-pointer rounded-full w-7 h-7 bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${t.profile?.imageUrl})` }}></div>
                             <div>
-                                <p>{t.profile.firstname}{" "}{t.profile.lastname})</p>
+                                <p className='text-sm'>{t.profile.firstname}{" "}{t.profile.lastname}</p>
+                                <p className='text-xs font-light'>{t.profile.email}</p>
                             </div>
                         </div>
                     ))
