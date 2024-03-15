b6f7c1a79055ab6b09a69bff5a25350fde75003a
sijirama <gbemilesanmi@gmail.com>
2024-03-15 20:55:57 +0100

    sidebar logo stuff

--- components/Layouts/TopBar.tsx
+++ components/Layouts/TopBar.tsx
@@ -11,7 +11,7 @@ export default function TopBar() {
     const { onOpen } = useModal()
     //TODO: get a better way to add team <Button onClick={() => onOpen("createProject")} className='hidden md:flex font-semibold px-3 gap-0.5 items-center justify-center'><MdOutlineAdd className='text-xl font-bold' /> Create Project</Button>
     return (
-        <nav className='w-full h-16 p-1 bg-white dark:bg-black flex items-center justify-end space-x-4 px-3 py-2'>
+        <nav className='w-full h-16 p-1 bg-white border-b-[0.1px] border-zinc-100 dark:bg-black flex items-center justify-end space-x-4 px-3 py-2'>
 
             <ToggleSideBar />
             <UserButton />
--- components/Layouts/_components/SidebarWidthControl.tsx
+++ components/Layouts/_components/SidebarWidthControl.tsx
@@ -3,32 +3,31 @@ import useSidebarStore from "@/store/SideBarStore";
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
-  TbLayoutSidebarRightCollapse,
-  TbLayoutSidebarLeftCollapse,
+    TbLayoutSidebarRightCollapse,
+    TbLayoutSidebarLeftCollapse,
 } from "react-icons/tb";
 import Logo from "@/components/custom/Logo";
 
 export default function SideBarControl() {
-  const { isCollapsed, toggleSidebarCollapse } = useSidebarStore();
-  return (
-    <div
-      className={`items-center hidden md:flex ${
-        isCollapsed ? "justify-center" : "justify-between"
-      } w-full`}
-    >
-      <div className={`${isCollapsed ? "hidden" : "flex-1"}`}>
-        <Logo className="w-24 h-9" />
-      </div>
-      <div
-        onClick={toggleSidebarCollapse}
-        className="p-2 px-3 rounded-md text-myPrimary dark:text-white cursor-pointer"
-      >
-        {isCollapsed ? (
-          <TbLayoutSidebarRightCollapse className="w-6 h-6" />
-        ) : (
-          <TbLayoutSidebarLeftCollapse className="w-6 h-6" />
-        )}
-      </div>
-    </div>
-  );
+    const { isCollapsed, toggleSidebarCollapse } = useSidebarStore();
+    return (
+        <div
+            className={`items-center hidden md:flex ${isCollapsed ? "justify-center" : "justify-between"
+                } w-full`}
+        >
+            <div className={`${isCollapsed ? "hidden" : "flex-1 flex items-center justify-start"}`}>
+                <Logo className="w-20 h-9" />
+            </div>
+            <div
+                onClick={toggleSidebarCollapse}
+                className="p-2 px-3 rounded-md text-myPrimary dark:text-white cursor-pointer"
+            >
+                {isCollapsed ? (
+                    <TbLayoutSidebarRightCollapse className="w-6 h-6" />
+                ) : (
+                    <TbLayoutSidebarLeftCollapse className="w-6 h-6" />
+                )}
+            </div>
+        </div>
+    );
 }
