3da9e6fdead0661e4bb00f2983a997bf5adba51f
sijirama <gbemilesanmi@gmail.com>
2024-03-19 12:06:21 +0100

    edit task details again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,8 +72,8 @@ export default withUt({
                 },
             },
             animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
+                "accordion-down": "accordion-down 0.4s ease-out",
+                "accordion-up": "accordion-up 0.4s ease-out",
             },
         },
     },
